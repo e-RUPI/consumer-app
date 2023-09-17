4ab9bd8516ca814ae311557c21eccf69b2ad1aa8
Harsh <harsh.gupta2211@gmail.com>
2023-09-17 17:54:04 +0530

    initial commit

---
+++ app/_layout.js
@@ -0,0 +1,17 @@
+import { Stack } from "expo-router";
+
+export default function Layout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#3289ed",
+        },
+        headerTintColor: "#f073d1",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    ></Stack>
+  );
+}
---
+++ app/index.js
@@ -0,0 +1,36 @@
+import { Link, Stack } from "expo-router";
+import { Image, Text, View } from "react-native";
+
+function LogoTitle() {
+  return (
+    <Image
+      style={{ width: 25, height: 25 }}
+      source={{ uri: "https://reactnative.dev/img/tiny_logo.png" }}
+    />
+  );
+}
+
+export default function Home() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Stack.Screen
+        options={{
+          // https://reactnavigation.org/docs/headers#setting-the-header-title
+          title: "My home",
+          // https://reactnavigation.org/docs/headers#adjusting-header-styles
+          headerStyle: { backgroundColor: "#73f077" },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+          // https://reactnavigation.org/docs/headers#replacing-the-title-with-a-custom-component
+          // headerTitle: (props) => <LogoTitle {...props} />,
+        }}
+      />
+      <Text>Home Screen</Text>
+      <Link href={{ pathname: "page1", params: { name: "Bacon" } }}>
+        Go to Page 1
+      </Link>
+    </View>
+  );
+}
